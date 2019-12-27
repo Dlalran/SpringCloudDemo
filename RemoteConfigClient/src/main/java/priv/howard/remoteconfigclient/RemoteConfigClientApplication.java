41455a42c3c44a07941434595e98ff27898a2501
howard <604876547@qq.com>
2019-12-28 02:05:49 +0800

    Spring Cloud Demo

---
+++ RemoteConfigClient/src/main/java/priv/howard/remoteconfigclient/RemoteConfigClientApplication.java
@@ -0,0 +1,11 @@
+package priv.howard.remoteconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteConfigClientApplication.class, args);
+    }
+}
